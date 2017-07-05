ab0aa7d48b0efa5467c027d49316815929dae89d
tom-s <thom.schell@gmail.com>
2017-07-05 14:34:08 +0200

    added word detector

--- src/Demo.js
+++ src/Demo.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import SpeechRecognizer from 'components/speechRecognizer'
+import WordDetector from 'components/wordDetector'
 
 const App = () =>
   <div>
     Speech recognizer:
-    <SpeechRecognizer onResult={(res) => console.log("res", res)} onTempResult={(res) => console.log("temp res", res)}
+    {/*<SpeechRecognizer onResult={(res) => console.log("res", res)} onTempResult={(res) => console.log("temp res", res)}
       onError={(error) => console.log("error", error)}
-    />
+    />*/}
+    <WordDetector word='OK' onSuccess={(res, attempts) => {
+      console.log("success", res, attempts)
+    }} onFailure={(res, attempts) => {
+      console.log("failure", res, attempts)
+    }} />
   </div>
 
 export default App
